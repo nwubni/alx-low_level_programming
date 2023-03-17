c224e61b4c523ed26796d8f038eb88226fe45927
Nakuru Wubni <nwubni@gmail.com>
2023-03-17 12:40:22 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n everytime
+ * prints all English alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+        	putchar(ch);
+        
+	printf("\n");
+	
+	return (0);
+}
