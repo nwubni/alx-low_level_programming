29e92e2693be70a85e669e8fb34b10752d431dfe
Nakuru Wubni <nwubni@gmail.com>
2023-03-17 12:59:18 +0100

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - task
+ * prints all English alphabets except e and q
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if(ch != 'e' && ch != 'q')
+			putchar(ch);
+	printf("\n");
+	return (0);
+}
