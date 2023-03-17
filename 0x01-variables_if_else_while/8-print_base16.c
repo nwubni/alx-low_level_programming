8c9a0024e9ab0e2382e45748b49d0ba0c1e82406
Nakuru Wubni <nwubni@gmail.com>
2023-03-17 13:28:47 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - task
+ * prints all Hexadecimal numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i <= 10; i++)
+		putchar(ch + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
