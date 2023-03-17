5f0282626085042383933aa139e1bd583936265a
Nakuru Wubni <nwubni@gmail.com>
2023-03-17 13:05:13 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - task
+ * prints all Arabic numberals
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
