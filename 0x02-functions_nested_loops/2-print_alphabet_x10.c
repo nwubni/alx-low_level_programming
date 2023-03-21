0b58dfddbdf6c5e969c5fc44217cca4534a83e96
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 13:21:07 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to  print all lower case English alphabets
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; letter <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
