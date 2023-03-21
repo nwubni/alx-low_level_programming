0115a7e23ca35c67823c99998c537358685087c0
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 12:56:25 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
-void print_alphabet(void);
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+
+/**
+ *main - prints English alphabets
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
+#endif
