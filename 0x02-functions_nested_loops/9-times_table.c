9e0bbba878f3a15115b624c32bef3c0c48733aa4
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 15:32:39 +0100

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - Prints times table
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i, j, n;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for(j = 0; j <= 9; j++)
+		{
+			n = i * j;
+			_putchar(n + '0');
+			_putchar(' ');
+
+			if(n <= 9)
+				_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
