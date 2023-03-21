dbdb5cd1282437946fedb7e9d8acc8c74fe0a034
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 13:26:51 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,7 @@
 
 /**
  * print_alphabet_x10 - function to  print all lower case 
- *English alphabets ten times on a new line
- *
+ * English alphabets ten times on a new line
  * Return: void
  */
 void print_alphabet_x10(void)
