58a162a8d2bc91043df6fc5eb9447e6be17a8872
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 14:03:58 +0100

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of n
+ * @n: the parameter
+ * Return: true of false
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+
+	return (0);
+}
