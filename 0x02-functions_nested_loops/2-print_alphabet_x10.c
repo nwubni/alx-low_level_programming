242e1a15b58afe47e28cd3303e6cad27d47ee1d3
Nakuru Wubni <nwubni@gmail.com>
2023-03-21 13:33:30 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char letter;
 
 	for (i = 1; i <= 10; i++)
 	{
-		print_alphabet();
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
 	}
 }
