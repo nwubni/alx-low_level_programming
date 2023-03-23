611310f9d11b23652d31e736653bc07d265f1b72
Nakuru Wubni <nwubni@gmail.com>
2023-03-23 15:39:51 +0100

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main- main function
+* Return: 0
+*/
+int main(void)
+{
+	long int n, i;
+
+	n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+
+	printf("%ld\n", i);
+
+	return (0);
+}
