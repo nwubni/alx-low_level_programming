8675d3b21c210bca1dcb647005897038470f4783
Nakuru Wubni <nwubni@gmail.com>
2023-03-23 13:01:36 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Takes two parameters and returns their product
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
