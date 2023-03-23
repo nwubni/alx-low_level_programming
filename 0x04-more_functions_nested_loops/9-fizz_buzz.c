5e453dc0170334e6e43946da2abf04476be82ea3
Nakuru Wubni <nwubni@gmail.com>
2023-03-23 14:37:15 +0100

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main- main program
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+
+		if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+
+		printf("%d ", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
