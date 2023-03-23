4bb5f5253e026c5381827e33231eac760f75fecf
Nakuru Wubni <nwubni@gmail.com>
2023-03-23 14:59:26 +0100

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_triangle- prints triangle with the character #
+* @size: dimension
+* Return: no return
+*/
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+			_putchar(' ');
+
+		for (k = 1; j <= i; j++)
+			_putchar('#');
+
+		if (i < size)
+			_putchar('\n');
+	}
+
+	_putchar('\n');
+}
