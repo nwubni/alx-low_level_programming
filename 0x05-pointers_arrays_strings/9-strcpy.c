d0423481ba477d77e51b545952184c9db33414d1
Nakuru Wubni <nwubni@gmail.com>
2023-03-27 10:03:01 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcpy- Copies src to dest
+* @dest: Destination string
+* @src: Source string
+* Return: String
+*/
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return dest;
+}
