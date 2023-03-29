16ffaee4e803eb22df7d6e845f4c598c43cf461a
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 20:58:03 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalizes every first
+* letter of word in s
+* @s: Input string
+* Return: String
+*/
+char *cap_string(char *s)
+{
+	int i, cap;
+
+	cap = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (cap && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+			cap = 0;
+			continue;
+		}
+
+		if (s[i] == ' ' || s[i] = '\t' || s[i] == '\n' || s[i] == '!' || s[i] == '.')
+			cap = 1;
+	}
+
+	return (s);
+}
