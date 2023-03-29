ff62a91e8170dde2db8c50d83fe72f70eab819a8
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 09:48:22 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcat - Concatenates two strings
+* @dest: Destination string
+* @src: Source string
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
