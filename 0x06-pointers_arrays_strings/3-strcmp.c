0c4b1188d911ac61b9ef749a1044ed7b3cfeaffc
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 19:30:08 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncpy - Compares two strings
+* @s1: First string
+* @s2: Second string
+* Return: Integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2 && *s1 == '\0')
+		return (0);
+
+	if(!*s1 && *s2)
+		return (-1);
+
+	return (1);
+}
