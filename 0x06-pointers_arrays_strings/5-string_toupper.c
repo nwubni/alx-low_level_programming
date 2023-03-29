1bb818a96c12eefa74f64f8dad439dd36d1a977f
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 20:26:12 +0100

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
 * string_toupper - Changes lower case letters
@@ -12,8 +11,8 @@ char *string_toupper(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-		if (islower(s[i]))
-			s[i] = toupper(s[i]);
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
 	return (s);
 }
