339389cb2303ee54f6b4aed91f46a344fb6fbeb8
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 18:47:42 +0100

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strncpy - Copies part of src to dest
+* @dest: Destination string
+* @src: Source string
+* @n: Number of characters from src
+* Return: String
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*dest && *src && n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
