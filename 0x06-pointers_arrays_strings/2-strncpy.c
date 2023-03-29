aa002c2b519da6ae1253bf1d63267c19e0b6cc57
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 18:59:49 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * _strncpy - Copies part of src to dest
@@ -9,13 +10,6 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
+	strncpy(dest, src, n);
 	return (dest);
 }
