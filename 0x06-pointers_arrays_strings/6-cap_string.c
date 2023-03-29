7a465c926fd57c236dc1703d0b054b7ac70b4544
Nakuru Wubni <nwubni@gmail.com>
2023-03-29 21:01:04 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,8 @@ char *cap_string(char *s)
 			continue;
 		}
 
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '!' || s[i] == '.')
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' 
+		|| s[i] == '!' || s[i] == '.')
 			cap = 1;
 	}
 
