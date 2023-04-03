d0bb025e6afa563ec1bca5bd8b43a5005a0a33ff
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 13:37:41 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] == c)
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
 			return (s + i);
 
-	if (s[i] == c)
+	if (*(s + i) == c)
 		return (s + i);
 
-	return (NULL);
+	return ('\0');
 }
