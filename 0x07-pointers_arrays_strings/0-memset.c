e09679ebc59cb43d2d21b467a104a5db7a42e3a1
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 12:14:16 +0100

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _memset - Fills memory
+* @s: String parameter
+* @b: Value to fill memory
+* @n: Number of memory to fill
+* Return: String
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return s;
+}
