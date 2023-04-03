eb94de326708419ebe56b24168c25f1f50ffce30
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 12:37:03 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strchr - Find firs c in s
+* @s: Input string parameter
+* @c: Character to find
+* Return: String
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (s[i]);
+
+	return (NULL);
+}
