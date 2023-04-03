5a0826d872d1be9f2f68e8c71db3d2eb6e7e7048
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 15:59:25 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strstr - Finds needle in haystack
+* @haystack: Haystack string parameter
+* @needle: Needle string parameter
+* Return: Pointer to first occurence
+* of needle in haystack
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, k, h_len, n_len;
+
+	h_len = strlen(haystack);
+	n_len = strlen(needle);
+
+	for (i = 0; i < h_len; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0, k = i; k < h_len && j < n_len; j++, k++)
+			{
+				if (haystack[k] != needle[j])
+					break;
+			}
+
+			if (j == n_len)
+				return (haystack + i);
+		}
+	}
+
+	return (NULL);
+}
