5ab07424cc6066fddcca5c44668d57e1b8db5dfd
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 15:26:42 +0100

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String parameter.
+ * @accept: Prefix parameter.
+ * Return: Integer.
+ */
+ 
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *p = s;
+
+	while (*p != '\0' && strchr(accept, *p) != NULL) {
+		count++;
+		p++;
+	}
+
+	return count;
+}
