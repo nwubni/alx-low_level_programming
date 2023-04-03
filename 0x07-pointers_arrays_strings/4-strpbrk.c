b52d8c5120deee0f193b4b68fedcc4ccbf960a37
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 15:42:27 +0100

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strpbrk - function locates the first
+* occurrence in the string s of any of
+* the bytes in the string accept
+* @s: String parameter
+* @accept: Accept string parameter
+* Return: Pointer to the byte in s
+* that matches one of the bytes in accept
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0') {
+		if (strchr(accept, *s) != NULL)
+			return (s);
+
+		s++;
+	}
+
+	return (NULL);
+}
