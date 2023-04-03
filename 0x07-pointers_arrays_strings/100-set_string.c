696515ef134fdfa04c142f20b648e0a26ba54b07
Nakuru Wubni <nwubni@gmail.com>
2023-04-03 17:29:41 +0100

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string - Sets one string to another
+* @s: String to replace
+* to: Replace value
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
