401e80bd6b4a1017f9355f8e55462de63953147a
Nakuru Wubni <nwubni@gmail.com>
2023-04-11 12:08:12 +0100

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strdup - Duplicates string
+* @str: String to duplicate
+* Return: String
+*/
+
+char *_strdup(char *str)
+{
+	int n;
+	char *dup;
+
+	n = strlen(str);
+	dup = malloc(n * sizeof(char));
+
+	strcpy(dup, str);
+
+	return (dup);
+}
