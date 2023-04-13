f8959f3360d70054a05483a57278411690e9af8a
Nakuru Wubni <nwubni@gmail.com>
2023-04-13 09:59:37 +0100

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - Checks and allocates memory
+* @b: Memory size
+* Return: Pointer to alocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return p;
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(int c);
+void *malloc_checked(unsigned int b);
+
+#endif
