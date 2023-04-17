4d68debcf06a892f65442751e1d4d8afbd070485
Nakuru Wubni <nwubni@gmail.com>
2023-04-17 14:09:02 +0100

    How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "doh.h"
+#include <stdlib.h>
+
+/**
+* free_dog - Function frees d
+* @d: dog_t* parameter
+* Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d) {
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
