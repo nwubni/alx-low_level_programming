8e5fa4f479d598db79ce208b4ff5fca9ceeabe80
Nakuru Wubni <nwubni@gmail.com>
2023-04-17 12:56:50 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Longer description
+ */
+ 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
