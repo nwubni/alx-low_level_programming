4414fd4454ead5f16058d23e4d04d33bbe375575
Nakuru Wubni <nwubni@gmail.com>
2023-04-19 13:21:02 +0100

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,14 +7,14 @@
 * @action: Pointer function
 * Return: void
 */
- 
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int a;
 
 	if (!array || !action)
 		return;
-	
-		for (a = 0; a < size; a++)
-			action(array[a]);
+
+	for (a = 0; a < size; a++)
+		action(array[a]);
 }
