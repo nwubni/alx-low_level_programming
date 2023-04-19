bb9114e61c830260de633116f2c23527926dc973
Nakuru Wubni <nwubni@gmail.com>
2023-04-19 13:31:31 +0100

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+#include <stdlib.h>
+
 void _putchar(int);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
