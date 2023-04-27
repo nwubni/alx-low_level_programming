0a27270d069f5155e6015e2bfde656bcd2a883dd
Nakuru Wubni <nwubni@gmail.com>
2023-04-27 14:16:17 +0100

    Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+	hello: db 'Hello, Holberton', 0
+	format: db '%s', 10, 0
+
+section .text
+_start:
+	mov rdi, format
+	mov rsi, hello
+	xor rax, rax
+
+	call printf
+
+	xor rdi, rdi
+	mov rax, 60
+	syscall
+
