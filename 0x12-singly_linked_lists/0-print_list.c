5fc4438560dc6c8d5eb24a1e155428203773cf4b
Nakuru Wubni <nwubni@gmail.com>
2023-04-27 12:38:35 +0100

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+* print_list - Prints a linked list
+* @list_t: Head of linked list
+* Return: Size of linked list
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *curr = h;
+	size_t count = 0;
+
+	while (curr)
+	{
+		printf("[%d] %s\n", curr->len, (curr->str ? curr->str : "(nil)"));
+		curr = curr->next;
+		count++;
+	}
+
+	return (count);
+}
