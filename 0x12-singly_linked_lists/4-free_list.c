133c82a6b1d272aaa16a8b5ca20a303aed3d4f1d
Nakuru Wubni <nwubni@gmail.com>
2023-04-27 13:54:34 +0100

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_list - Frees a linked list's memory
+* @head: Head of linked list
+* Return: void
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
