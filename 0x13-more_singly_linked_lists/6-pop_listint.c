898a175271616fa0547582379f3022f1ee179ea0
Nakuru Wubni <nwubni@gmail.com>
2023-04-29 20:25:39 +0100

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* pop_listint - Removes first node in list
+* @head: Head of linked list
+* Return: Value of head
+*/
+
+int pop_listint(listint_t **head)
+{
+	int val = 0;
+
+	if (head && *head)
+	{
+		val = (*head)->n;
+		*head = (*head)->next;
+	}
+
+	return (val);
+}
