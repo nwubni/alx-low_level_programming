fcc2dd78027e9a163cf65bd4beeff7b4e7f6a29c
Nakuru Wubni <nwubni@gmail.com>
2023-04-29 13:13:12 +0100

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Frees memory of a list
+* @head: Head of list passed by reference
+* Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head;
+	listint_t *temp;
+
+	while (curr)
+	{
+		temp = curr;
+		curr = curr->next;
+
+		free(temp);
+	}
+}
