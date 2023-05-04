e78bb58b362827f32000324212961f1f227979f0
Nakuru Wubni <nwubni@gmail.com>
2023-05-04 12:53:42 +0100

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: binary number as a string
+* Return: Unsigned Integer.
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result <<= 1;
+
+		if (*b == '1')
+			result += 1;
+
+		b++;
+	}
+
+	return (result);
+}
+
