8d42f4511127df75bfae98dbdef0307c8d8b2944
Nakuru Wubni <nwubni@gmail.com>
2023-05-08 19:54:44 +0100

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,14 +19,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
 
 		wr = write(fd, text_content, len);
 
-		if (wr == -1) {
+		if (wr == -1)
+		{
 			close(fd);
 			return (-1);
 		}
