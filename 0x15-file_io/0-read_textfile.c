2bf55d909cfd8a66107ce8898ad378f04f82508d
Nakuru Wubni <nwubni@gmail.com>
2023-05-08 16:25:29 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,16 +9,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
+	int fd;
 	char buffer[letters];
-	ssize_t bytes_read = read(fd, buffer, letters);
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
+	bytes_read = read(fd, buffer, letters);
 
 	if (bytes_read == -1)
 	{
@@ -26,6 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	close(fd);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
