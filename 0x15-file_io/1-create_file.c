702fb2cbd4696a54308e84bec23bba5b5f03952b
Nakuru Wubni <nwubni@gmail.com>
2023-05-08 19:22:15 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,8 @@
 * Return: Integer to indicate success
 */
 
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 	int fd, len = 0, wr;
 
 	if (filename == NULL)
