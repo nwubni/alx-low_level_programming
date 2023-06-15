c577d32dd4f41277d820ad99ca8644e1d0fb2a0d
Nakuru Wubni <nwubni@gmail.com>
2023-06-15 15:43:32 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,41 +12,39 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int pos = 0;
-	dlistint_t *new_node, *curr = *h;
-
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	dlistint_t *new_node = NULL, *curr = *h;
 
 	while (curr && curr->prev)
 		curr = curr->prev;
+	
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
 
-	while (curr && curr->next && pos < (idx - 1))
+	while (curr && idx)
 	{
+		if (pos == (idx - 1))
+		{
+			if (!curr->next)
+				new_node = add_dnodeint_end(h, n);
+			else {
+				new_node = malloc(sizeof(dlistint_t));
+
+				if (new_node)
+				{
+					new_node->n = n;
+					new_node->prev = curr;
+					new_node->next = curr->next;
+					curr->next->prev = new_node;
+					curr->next = new_node;
+				}
+			}
+
+			break;
+		}
+		
 		curr = curr->next;
 		pos++;
 	}
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		return (*h);
-	}
-	if (curr && pos == (idx - 1))
-	{
-		new_node->prev = curr;
-		new_node->next = curr->next;
-		if (curr->next)
-			curr->next->prev = new_node;
-
-		curr->next = new_node;
-		return (new_node);
-	}
-	free(new_node);
-	return (NULL);
+	
+	return (new_node);
 }
