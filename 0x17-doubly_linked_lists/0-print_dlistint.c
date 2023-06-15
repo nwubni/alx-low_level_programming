10ee7fc2086182b49d3bc94d1a12a603fe9f66b9
Nakuru Wubni <nwubni@gmail.com>
2023-06-15 13:13:04 +0100

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+* print_dlistint - Prints doubly linked list
+* @h: Head of doubly linked list
+* Return: Number of nodes in doubly linked list
+*/
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
