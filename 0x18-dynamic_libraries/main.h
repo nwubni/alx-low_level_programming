7d4d0e7c7766057f97537019d3127dc901cb037a
Nakuru Wubni <nwubni@gmail.com>
2023-06-19 12:45:10 +0100

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define EXIT_SUCCESS 0
+/*#define EXIT_SUCCESS 0*/
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,10 +24,10 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-int add(int a, int b);
+/*int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
 int div(int a, int b);
-int mod(int a, int b);
+int mod(int a, int b);*/
 
 #endif
