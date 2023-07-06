0fd8b6102eb4317ebf8f69cc3933a40423e2fd40
Nakuru Wubni <nwubni@gmail.com>
2023-07-06 22:53:16 +0100

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+* get_value - Gets key value in linked list
+* @h: Head of linked list
+* @key: Key to retreive its value
+* Return: Size of linked list
+*/
+
+char *get_value(const hash_node_t *h, const char *key)
+{
+	const hash_node_t *curr = h;
+
+	while (key && curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+
+		curr = curr->next;
+	}
+
+	return (NULL);
+}
+
+/**
+* hash_table_get - Gets a key's value
+* @ht: Hash table
+* @key: Key parameter
+* Return: String
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+
+	return (get_value(ht->array[index], key));
+}
+
