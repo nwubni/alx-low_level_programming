923f0f239e3054a6be7cc7a688892fcdf5f63231
Nakuru Wubni <nwubni@gmail.com>
2023-07-06 19:42:52 +0100

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - Creates an array of hash_table_t
+* @size: Size of array
+* Return: Pointer to allocated array.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *hash_table = NULL;
+
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (!hash_table)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	return (hash_table);
+}
